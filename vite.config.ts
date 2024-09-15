6fec1f9c2eed8aca3f2a4adef3d78dc7e8dfbfb3
Tyler Fisher <tyler@tylerjfisher.com>
2024-09-14 21:34:07 -0400

    vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+/// <reference types="vitest" />
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import react from "@vitejs/plugin-react";
 import { devServer } from "react-router-hono-server/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
@@ -12,13 +14,20 @@ export default defineConfig({
 	},
 	plugins: [
 		devServer(),
-		remix({
-			future: {
-				v3_fetcherPersist: true,
-				v3_relativeSplatPath: true,
-				v3_throwAbortReason: true,
-			},
-		}),
+		process.env.VITEST
+			? remix({
+					future: {
+						v3_fetcherPersist: true,
+						v3_relativeSplatPath: true,
+						v3_throwAbortReason: true,
+					},
+				})
+			: react(),
 		tsconfigPaths(),
 	],
+	test: {
+		environment: "happy-dom",
+		// Additionally, this is to load ".env.test" during vitest
+		env: loadEnv("test", process.cwd(), ""),
+	},
 });
