b9e3b41f7179a911bb320acd1ec24d96a56cf88f
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-24 12:45:21 -0400

    Clear search query button

--- app/components/SearchField.tsx
+++ app/components/SearchField.tsx
@@ -1,19 +1,31 @@
 import { Button, TextField } from "@radix-ui/themes";
 import { useSearchParams } from "@remix-run/react";
-import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import { Cross2Icon, MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 const SearchField = () => {
-	const [searchParams] = useSearchParams();
+	const [searchParams, setSearchParams] = useSearchParams();
+	const query = searchParams.get("query") || "";
+
+	function setSearchParam(param: string, value: string) {
+		setSearchParams((prev) => {
+			prev.set(param, value);
+			return prev;
+		});
+	}
 
 	return (
-		<TextField.Root
-			name="query"
-			type="text"
-			defaultValue={searchParams.get("query") || ""}
-		>
+		<TextField.Root name="query" type="text" defaultValue={query}>
 			<TextField.Slot>
 				<MagnifyingGlassIcon height="16" width="16" />
 			</TextField.Slot>
+			{query && (
+				<TextField.Slot>
+					<Cross2Icon
+						cursor="pointer"
+						onClick={() => setSearchParam("query", "")}
+					/>
+				</TextField.Slot>
+			)}
 			<TextField.Slot>
 				<Button type="submit" variant="ghost">
 					Search
