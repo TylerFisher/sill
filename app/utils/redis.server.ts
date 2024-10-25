0d4b4c083d5db4e334d7a4c111a19033ea497c29
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-25 00:00:07 -0400

    actually use the redis url

--- app/utils/redis.server.ts
+++ app/utils/redis.server.ts
@@ -1,9 +1,9 @@
 import { Redis } from "ioredis";
 
 export const connection = () => {
-	// const redisUrl = process.env.REDIS_URL;
-	// if (redisUrl == null) throw new Error("REDIS_URL must be defined");
-	const connect = new Redis({
+	const redisUrl = process.env.REDIS_URL;
+	if (redisUrl == null) throw new Error("REDIS_URL must be defined");
+	const connect = new Redis(redisUrl, {
 		maxRetriesPerRequest: null,
 	});
 	return connect;
