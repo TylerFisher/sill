fac45ee18b6e49c692b93952924795a1d16d6806
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-26 16:01:00 -0400

    fix jwks route as well

--- app/routes/jwks[.]json.ts
+++ app/routes/jwks[.]json.ts
@@ -2,5 +2,5 @@ import { createOAuthClient } from "~/server/oauth/client";
 
 export const loader = async () => {
 	const oauthClient = await createOAuthClient();
-	return Response.json(oauthClient.jwks);
+	return oauthClient.jwks;
 };
