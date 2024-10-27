8f3446e04a563689cec538076a662261f6209a85
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-27 16:26:48 -0400

    cache the bluesky json files

--- app/routes/jwks[.]json.ts
+++ app/routes/jwks[.]json.ts
@@ -1,4 +1,10 @@
+import type { HeadersFunction } from "@vercel/remix";
 import { createOAuthClient } from "~/server/oauth/client";
+
+export const headers: HeadersFunction = () => ({
+	"Content-Type": "application/json",
+	"Cache-Control": "public, max-age=3600",
+});
 
 export const loader = async () => {
 	const oauthClient = await createOAuthClient();
