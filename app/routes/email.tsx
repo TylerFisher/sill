9eddd9f871a67fa7a468ff81569995effd9d8b43
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-30 15:36:06 -0400

    fix the email route

--- app/routes/email.tsx
+++ app/routes/email.tsx
@@ -11,6 +11,7 @@ import { eq } from "drizzle-orm";
 import { emailSettings } from "~/drizzle/schema.server";
 import {
 	Form,
+	json,
 	useActionData,
 	useSearchParams,
 	Link,
@@ -32,7 +33,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 		where: eq(emailSettings?.userId, userId),
 	});
 
-	return { currentSettings };
+	return json({ currentSettings });
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
