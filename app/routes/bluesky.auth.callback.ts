7f7a6286ce12c75d36acebb30f98dabb433d7766
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-29 21:07:51 -0400

    try to handle dpop better in callback

--- app/routes/bluesky.auth.callback.ts
+++ app/routes/bluesky.auth.callback.ts
@@ -1,4 +1,5 @@
 import { type LoaderFunctionArgs, redirect } from "@vercel/remix";
+
 import { createOAuthClient } from "~/server/oauth/client";
 import { Agent } from "@atproto/api";
 import { db } from "~/drizzle/db.server";
@@ -6,35 +7,15 @@ import { uuidv7 } from "uuidv7-js";
 import { requireUserId } from "~/utils/auth.server";
 import { blueskyFetchQueue } from "~/utils/queue.server";
 import { blueskyAccount } from "~/drizzle/schema.server";
-import {
-	OAuthCallbackError,
-	OAuthResponseError,
-	type OAuthSession,
-} from "@atproto/oauth-client-node";
 
 export async function loader({ request }: LoaderFunctionArgs) {
 	const userId = await requireUserId(request);
+	const oauthClient = await createOAuthClient();
+
 	try {
-		const oauthClient = await createOAuthClient();
-		let oauthSession: OAuthSession | null = null;
-		try {
-			const session = await oauthClient.callback(
-				new URL(request.url).searchParams,
-			);
-			oauthSession = session.session;
-		} catch (error) {
-			if (error instanceof OAuthResponseError) {
-				const session = await oauthClient.callback(
-					new URL(request.url).searchParams,
-				);
-				oauthSession = session.session;
-			}
-		}
-
-		if (!oauthSession) {
-			throw new Error("OAuth session couldn't be created");
-		}
-
+		const { session: oauthSession } = await oauthClient.callback(
+			new URL(request.url).searchParams,
+		);
 		const agent = new Agent(oauthSession);
 		const profile = await agent.getProfile({
 			actor: oauthSession.did,
@@ -53,6 +34,24 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 		return redirect("/connect");
 	} catch (error) {
+		const { session: oauthSession } = await oauthClient.callback(
+			new URL(request.url).searchParams,
+		);
+		const agent = new Agent(oauthSession);
+		const profile = await agent.getProfile({
+			actor: oauthSession.did,
+		});
+		await db.insert(blueskyAccount).values({
+			id: uuidv7(),
+			did: oauthSession.did,
+			handle: profile.data.handle,
+			userId: userId,
+			service: oauthSession.serverMetadata.issuer,
+		});
+
+		blueskyFetchQueue.add(`${userId}-bluesky-fetch`, {
+			userId,
+		});
 		console.error("Bluesky OAuth Error", { error: String(error) });
 		return redirect("/connect");
 	}
