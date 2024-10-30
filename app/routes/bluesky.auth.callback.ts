1bd6d25b4dcf4d8e9fa0baf0db506f0e68e44e8c
Tyler Fisher <tyler@tylerjfisher.com>
2024-10-29 20:17:35 -0400

    use bluesky handle resolution library

--- app/routes/bluesky.auth.callback.ts
+++ app/routes/bluesky.auth.callback.ts
@@ -35,11 +35,11 @@ export async function loader({ request }: LoaderFunctionArgs) {
 				)
 			) {
 				const client = await createOAuthClient();
-				const { userId, handle } = JSON.parse(error.state || "");
-				const url = await client.authorize(handle, {
+				const { userId, did } = JSON.parse(error.state || "");
+				const url = await client.authorize(did, {
 					state: JSON.stringify({
 						userId,
-						handle,
+						did,
 					}),
 				});
 
--- app/routes/bluesky.auth.ts
+++ app/routes/bluesky.auth.ts
@@ -2,25 +2,32 @@ import { type ActionFunctionArgs, redirect } from "@vercel/remix";
 import { OAuthResponseError } from "@atproto/oauth-client-node";
 import { createOAuthClient } from "~/server/oauth/client";
 import { requireUserId } from "~/utils/auth.server";
+import { HandleResolver } from "@atproto/identity";
+
+const resolver = new HandleResolver();
 
 export const action = async ({ request }: ActionFunctionArgs) => {
 	const userId = await requireUserId(request);
 	const data = await request.formData();
 	const handle = data.get("handle");
-	const state = JSON.stringify({ userId, handle });
 	if (typeof handle !== "string") {
 		throw new Error("Invalid handle");
 	}
+	const did = await resolver.resolve(handle);
+	if (!did) {
+		throw new Error("Failed to resolve handle");
+	}
+	const state = JSON.stringify({ userId, did });
 	const oauthClient = await createOAuthClient();
 	try {
-		const url = await oauthClient.authorize(handle, {
+		const url = await oauthClient.authorize(did, {
 			scope: "atproto transition:generic",
 			state,
 		});
 		return redirect(url.toString());
 	} catch (error) {
 		if (error instanceof OAuthResponseError) {
-			const url = await oauthClient.authorize(handle, {
+			const url = await oauthClient.authorize(did, {
 				scope: "atproto transition:generic",
 			});
 			return redirect(url.toString());
