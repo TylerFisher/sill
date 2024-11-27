61c5a3cb3ca3267ccfd6e08cf761474fb309ec76
Tyler Fisher <tyler@tylerjfisher.com>
2024-11-26 21:40:22 -0500

    remove banner, i think we're back

--- app/components/nav/Layout.tsx
+++ app/components/nav/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children, hideNav }: LayoutProps) => {
 				sm: "5",
 			}}
 		>
-			<div
+			{/* <div
 				style={{
 					backgroundColor: "red",
 					color: "white",
@@ -33,7 +33,7 @@ const Layout = ({ children, hideNav }: LayoutProps) => {
 					Our email provider is down, preventing new users from signing up.
 					Please come back later
 				</p>
-			</div>
+			</div> */}
 			<div className={styles.wrapper}>
 				<Header headerClass={hideNav ? "onboarding-logo" : "mobile-logo"} />
 				{!hideNav && (
