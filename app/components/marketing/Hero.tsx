8864713ad88ec5515b2a11aae96d6bd3c32a436e
Tyler Fisher <tyler@tylerjfisher.com>
2024-12-07 11:05:13 -0500

    redo mobile marketing

--- app/components/marketing/Hero.tsx
+++ app/components/marketing/Hero.tsx
@@ -15,7 +15,17 @@ import styles from "./Hero.module.css";
 const Hero = () => {
 	return (
 		<Box className={styles["hero-wrapper"]}>
-			<Flex mb="8" direction="row" justify="center" align="center" gap="8">
+			<Flex
+				mb="8"
+				direction="row"
+				justify="center"
+				align="center"
+				wrap={{
+					initial: "wrap",
+					md: "nowrap",
+				}}
+				gap="8"
+			>
 				<Flex
 					direction="column"
 					justify="center"
@@ -32,7 +42,7 @@ const Hero = () => {
 							Top news shared by <strong>the people you trust</strong>
 						</Text>
 						<Text as="p" size="4" align="center">
-							Sill streamlines your{" "}
+							Sill finds the most popular links in your{" "}
 							<RLink href="https://bsky.app">Bluesky</RLink> and{" "}
 							<RLink href="https://joinmastodon.org">Mastodon</RLink> feeds to
 							give you a clear picture of what&rsquo;s happening.
