171c217db209995a9578ddace543c673ccdd0458
Tyler Fisher <tyler@tylerjfisher.com>
2025-06-23 16:24:00 +0200

    consoildate trial banner treatment

--- app/components/nav/TrialBanner.tsx
+++ app/components/nav/TrialBanner.tsx
@@ -6,6 +6,21 @@ import { daysRemaining } from "~/utils/misc";
 const TrialBanner = ({ endDate }: { endDate: Date }) => {
 	const remaining = daysRemaining(endDate);
 	const days = remaining === 1 ? "day" : "days";
+
+	const sillPlusText = (
+		<Text
+			color="yellow"
+			style={{
+				fontWeight: 900,
+				fontStyle: "italic",
+			}}
+		>
+			sill+
+		</Text>
+	);
+
+	const subscribeLink = <Link to="/settings/subscription">Subscribe now</Link>;
+
 	return (
 		<Flex
 			justify="center"
@@ -15,32 +30,7 @@ const TrialBanner = ({ endDate }: { endDate: Date }) => {
 			className={styles.banner}
 		>
 			<Text className={styles.text}>
-				<span className={styles.desktop}>
-					{remaining} {days} left in your{" "}
-					<Text
-						color="yellow"
-						style={{
-							fontWeight: 900,
-							fontStyle: "italic",
-						}}
-					>
-						sill+
-					</Text>{" "}
-					trial. <Link to="/settings/subscription">Subscribe now</Link>.
-				</span>
-				<span className={styles.mobile}>
-					{remaining} {days} of{" "}
-					<Text
-						color="yellow"
-						style={{
-							fontWeight: 900,
-							fontStyle: "italic",
-						}}
-					>
-						sill+
-					</Text>{" "}
-					left. <Link to="/settings/subscription">Subscribe now</Link>.
-				</span>
+				{remaining} {days} left in your {sillPlusText} trial. {subscribeLink}.
 			</Text>
 		</Flex>
 	);
